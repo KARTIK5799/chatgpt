050947f2da7c44221610652255f7126353941187
KARTIK5799 <thekartiksapkal@gmail.com>
2023-10-30 14:31:47 +0530

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgBtn  from './assets/message.svg'
+import Home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import rocket from './assets/rocket.svg'
 
 function App() {
   return (
     <div className="App">
-     .
+      <div className="sideBar">
+          <div className="upperSide">
+            <div className="upperSideTop">
+              <img src={gptLogo} alt="Logo" className="log" />
+              <span className="brand">ChatGPT </span>
+              </div>
+              <button className="midBtn"><img src={addBtn} alt="  " className="addBtn" />New Chat</button>
+            <div className="upperSideBottom">
+              <button className="query"><img src={msgBtn} alt="query" />What is Preograming?</button>
+              <button className="query"><img src={msgBtn} alt="query" />How to Use An Api</button>
+            </div>
+          </div>
+          <div className="lowerSide">
+             <div className="listItems">
+              <img src={Home} alt="" className="listItemsImg" />
+              Home
+             </div>
 
+             <div className="listItems">
+              <img src={saved} alt="" className="listItemsImg" />
+              Save
+             </div>
+
+             <div className="listItems">
+              <img src={rocket} alt="" className="listItemsImg" />
+              Upgrade 
+             </div>
+
+          </div>
+      </div>
+      <div className="main">
+
+      </div>
     </div>
   );
 }
